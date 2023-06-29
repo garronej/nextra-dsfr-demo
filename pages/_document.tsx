90dac7519734abb0fe8348b838b59b1b81ca4ac6
garronej <joseph.garrone@data.gouv.fr>
2023-06-29 12:59:58 +0200

    Must export the type of the link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,10 @@
 export { default } from "@codegouvfr/nextra-theme-dsfr/_document";
+
+import type Link from "next/link";
+import "@codegouvfr/react-dsfr/next-pagesdir"
+
+declare module "@codegouvfr/react-dsfr/next-pagesdir" {
+    interface RegisterLink {
+        Link: typeof Link;
+    }
+}
